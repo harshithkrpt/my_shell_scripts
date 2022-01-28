cc3c5a50a880ece4a860974bb1a504b165e47d92
harshithkrpt <harshith.krpt@gmail.com>
2022-01-29 02:17:56 +0530

    Adding Ubuntu Files

---
+++ ubuntu/run_this_to_install_all_required_software.sh
@@ -0,0 +1,14 @@
+
+sudo apt install snapd
+sudo snap install starship
+# Install GIT
+sudo apt install git
+# INSTALLING DRACULA THEME
+sudo apt-get install dconf-cli
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
+
+cd ~
+
+wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
